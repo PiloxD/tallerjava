4193cc642896efaf87c8b8ecb38283366a960fa2
Andrés Camilo Díaz Merlano <87391163+PiloxD@users.noreply.github.com>
2022-07-19 13:26:26 -0500

    Update Persona.java

--- java/exercise16/Persona.java
+++ java/exercise16/Persona.java
@@ -3,7 +3,7 @@ package com.taller.java.exercise16;
 public class Persona {
     private String nombre;
     private int edad;
-    private int DNI = 111111;
+    private int documento = 111111;
     private String sexo =  hombre;
     private Double peso;
     private Double estatura;
@@ -19,7 +19,7 @@ public class Persona {
     public Persona(String nombreNuevo, int edadNueva, int nuevoDNI, String sexoNuevo, Double pesoNuevo, Double estaturaNueva){
         nombre = nombreNuevo;
         edad = edadNueva;
-        DNI = nuevoDNI;
+        documento = nuevoDNI;
         sexo = sexoNuevo;
         peso = pesoNuevo;
         estatura = estaturaNueva;
